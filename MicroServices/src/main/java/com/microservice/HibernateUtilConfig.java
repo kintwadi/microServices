4132697c5d7f03b19c34efc5056e98b77d225e61
MabialaA <antmabiala@gmail.com>
2019-01-04 15:40:20 +0100

    c

--- MicroServices/src/main/java/com/microservice/HibernateUtilConfig.java
+++ MicroServices/src/main/java/com/microservice/HibernateUtilConfig.java
@@ -16,6 +16,7 @@ public class HibernateUtilConfig {
 
 	@Bean
 	public SessionFactory getSessionFactory() {
+		
 	    if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
 	        throw new NullPointerException("factory is not a hibernate factory");
 	    }
